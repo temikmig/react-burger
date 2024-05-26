a77dc8c26d7d1cb24d2570cb90f1be4c56a7ec23
temikmig <t97m@yandex.ru>
2024-05-26 17:42:02 +0300

    test commit

---
+++ src/components/app-header/app-header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppHeaderStyles from 'app-header.module.css';
+
+const AppHeader = () => {
+    return (
+        <h1>Header</h1>
+    );
+}
+
+export default AppHeader;
---
+++ src/components/app/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppStyles from './app.module.css';
+import AppHeader from '../app-header/app-header';
+
+export default function App() {
+    return (
+        <>
+            <AppHeader />
+        </>
+    );
+}
