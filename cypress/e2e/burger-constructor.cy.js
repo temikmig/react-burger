b64a8e5478e8c5ae3c0800750edbbbd3606bf154
temikmig <t97m@yandex.ru>
2024-08-11 04:17:59 +0300

    правка cypress

--- cypress/e2e/burger-constructor.cy.js
+++ cypress/e2e/burger-constructor.cy.js
@@ -1,6 +1,19 @@
+const modalWin = '[class^=modal_modalWin]';
+const modalClose = '[data-type="close_modal"]';
+const modalOverlay = '[class^=modal-overlay_modalOverlay]';
+
+const ingredientBun = '[data-type="ingredient_bun"]';
+const ingredientSauce = '[data-type="ingredient_sauce"]';
+const ingredientMain = '[data-type="ingredient_main"]';
+
+const burgerConstructorElementPrice = '[class^=constructor-element__price]';
+const burgerConstructorOrderPrice = '[class^=burger-constructor_burgerOrderPrice]';
+
+const burgerConstructorCont = '[data-type="burger_constructor_cont"]';
+
 describe('template spec', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/login');
+    cy.visit('login');
     cy.get('input').first().type('t97m@yandex.ru');
     cy.get('input').last().type('hesoyam');
     cy.get('button').click();
@@ -14,42 +27,42 @@ describe('template spec', () => {
 
   it('Первое нажатие на "Оформить заказ" при пустом конструкторе', () => {
     cy.get('button').contains('Оформить заказ').click();
-    cy.get('[class^=modal_modalWin]').should('contain', 'Произошла ошибка при получении данных');
-    cy.get('[data-type="close_modal"]').click();
+    cy.get(modalWin).should('contain', 'Произошла ошибка при получении данных');
+    cy.get(modalClose).click();
   });
 
   it('Нажатие на ингредиент', function(){
-    cy.get('[data-type="ingredient_bun"]').contains('Флюоресцентная булка R2-D3').first().click();
-    cy.get('[class^=modal_modalWin]').should('contain', 'Флюоресцентная булка R2-D3');
-    cy.get('[class^=modal-overlay_modalOverlay]').click();
+    cy.get(ingredientBun).contains('Флюоресцентная булка R2-D3').first().click();
+    cy.get(modalWin).should('contain', 'Флюоресцентная булка R2-D3');
+    cy.get(modalOverlay).click();
   });
 
   it('Перетягивание ингредиентов в конструктор', function(){
-    cy.get('[data-type="ingredient_bun"]').first().trigger("dragstart").trigger("dragleave");
-    cy.get('[data-type="burger_constructor_cont"]').trigger("dragenter").trigger("dragover").trigger("drop").trigger("dragend");
+    cy.get(ingredientBun).first().trigger("dragstart").trigger("dragleave");
+    cy.get(burgerConstructorCont).trigger("dragenter").trigger("dragover").trigger("drop").trigger("dragend");
  
-    cy.get('[data-type="ingredient_sauce"]').eq(1).trigger("dragstart").trigger("dragleave");
-    cy.get('[data-type="burger_constructor_cont"]').trigger("dragenter").trigger("dragover").trigger("drop").trigger("dragend");
+    cy.get(ingredientSauce).eq(1).trigger("dragstart").trigger("dragleave");
+    cy.get(burgerConstructorCont).trigger("dragenter").trigger("dragover").trigger("drop").trigger("dragend");
  
-    cy.get('[data-type="ingredient_main"]').eq(1).trigger("dragstart").trigger("dragleave");
-    cy.get('[data-type="burger_constructor_cont"]').trigger("dragenter").trigger("dragover").trigger("drop").trigger("dragend");
+    cy.get(ingredientMain).eq(1).trigger("dragstart").trigger("dragleave");
+    cy.get(burgerConstructorCont).trigger("dragenter").trigger("dragover").trigger("drop").trigger("dragend");
   
-    cy.get('[data-type="ingredient_main"]').eq(2).trigger("dragstart").trigger("dragleave");
-    cy.get('[data-type="burger_constructor_cont"]').trigger("dragenter").trigger("dragover").trigger("drop").trigger("dragend");
+    cy.get(ingredientMain).eq(2).trigger("dragstart").trigger("dragleave");
+    cy.get(burgerConstructorCont).trigger("dragenter").trigger("dragover").trigger("drop").trigger("dragend");
 
     let total = 0;
     let expectedTotal = 0;
 
-    cy.get('[class^=constructor-element__price]').each(price => {
+    cy.get(burgerConstructorElementPrice).each(price => {
         total = total + parseInt(price.text());
     }).then(() => {
-       cy.get('[class^=burger-constructor_burgerOrderPrice]').invoke('text').then(text => expectedTotal = +text).then(() => {
+       cy.get(burgerConstructorOrderPrice).invoke('text').then(text => expectedTotal = +text).then(() => {
           expect(total).equal(expectedTotal);
         });
     });
 
     cy.get('button').contains('Оформить заказ').click();
-    cy.get('[class^=modal_modalWin]', { timeout: 16000 }).should('contain', 'Ваш заказ начали готовить');
+    cy.get(modalWin, { timeout: 16000 }).should('contain', 'Ваш заказ начали готовить');
   });
 })
 
